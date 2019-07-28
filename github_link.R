2a0db84278cc6f85eab63f0f4a4a8e20d7efdf3d
Abhi <16918558+mailtoabhi15@users.noreply.github.com>
2019-07-28 21:56:38 +0530

    Update github_link.R

--- github_link.R
+++ github_link.R
@@ -1,3 +1,4 @@
 # Q3:Demo Shiny App around the UDPipe NLP workflow 
+library(shiny)
 source("https://raw.githubusercontent.com/mailtoabhi15/UDPipe_Shiny_app/master/dependency.R")
 runGitHub("UDPipe_Shiny_app","mailtoabhi15")
